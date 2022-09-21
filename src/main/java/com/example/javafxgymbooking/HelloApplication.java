cf081b29a367158e704d8e0879c9da2f8a149d61
DJ211285 <dj211285@reigate.ac.uk>
2022-09-21 15:55:27 +0100

    updated code 21/09/22

--- src/main/java/com/example/javafxgymbooking/HelloApplication.java
+++ src/main/java/com/example/javafxgymbooking/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome To The Gym Booking System!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/javafxgymbooking/HelloController.java
+++ src/main/java/com/example/javafxgymbooking/HelloController.java
@@ -2,13 +2,31 @@ package com.example.javafxgymbooking;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField Textfield1;
+
+    @FXML
+    private PasswordField PasswordField1;
+
+    @FXML
+    private Label Label1;
+
+    @FXML
+    private Button Button1;
+
+
+    //@FXML
+   // protected void onSignInButtonPress() {
+
+
     }
-}
+
+
+
